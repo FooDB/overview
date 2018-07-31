04764c5b5ce3bea2ff067b6851ba21496a3113ab
Shruti Rajagopal <shrutiprajagopal@gmail.com>
2018-07-30 19:54:18 -0700

    implements column element in rightCol

--- client/src/components/RightCol.jsx
+++ client/src/components/RightCol.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-
+import ColumnElement from './ColumnElement.jsx';
 const RightCol = props => (
   <div id="rightcol">
-    <div id="address">
-      <div>{props.addr}</div>
+
+    <div>
+      <ColumnElement id={'address'} name={'props.addr'} contents={''} />
     </div>
-
-    <div id="neighborhood">
-      <h2> Neighborhood </h2>
-      <div>{props.neighborhood}</div>
+    <div>
+      <ColumnElement
+        id={'neighborhood'}
+        name={'Neighborhood'}
+        contents={props.neighborhood}
+      />
     </div>
-
-    <div id="crossStreet">
-      <h2> Cross street </h2>
-      <div>{props.cross_street}</div>
+    <div>
+      <ColumnElement
+        id={'crossStreet'}
+        name={'Cross street'}
+        contents={props.cross_street}
+      />
     </div>
-
-    <div id="parking">
-      <h2> Parking details </h2>
-      <div>{props.parking}</div>
-
+    <div>
+      <ColumnElement
+        id={'parking'}
+        name={'Parking details'}
+        contents={props.parking}
+      />
     </div>
-
-    <div id="publicTransport">
-      <h2> Public transit </h2>
-      <div>{props.public_transport}</div>
+    <div>
+      <ColumnElement
+        id={'publicTransport'}
+        name={'Public transit'}
+        contents={props.public_transport}
+      />
     </div>
-
-    <div id="additional">
-      <h2> Additional </h2>
-      <div>{props.additional.join (', ')}</div>
+    <div>
+      <ColumnElement
+        id={'additional'}
+        name={'Additional'}
+        contents={props.additional.join (', ')}
+      />
     </div>
   </div>
 );
