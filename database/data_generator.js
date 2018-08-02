a6d1036f63018a947d78fc8ac5f69c82036edb92
Shruti Rajagopal <shrutiprajagopal@gmail.com>
2018-08-02 15:46:10 -0700

    docker complete

--- database/data_generator.js
+++ database/data_generator.js
@@ -346,4 +346,4 @@ const insertOverviews = () => {
   }
 };
 
-insertOverviews ();
+setTimeout (insertOverviews, 60000);
---
+++ database/schema.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS overviews;
+CREATE DATABASE overviews;
+USE overviews;
+
+CREATE TABLE General (
+    id int NOT NULL AUTO_INCREMENT,
+    restaurant_name VARCHAR(1000) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    telephone VARCHAR(1000) NOT NULL,
+    website VARCHAR(1000) NOT NULL,
+    chef VARCHAR(1000) NOT NULL,
+    avg_rating int NOT NULL,
+    num_ratings int NOT NULL,
+    style VARCHAR(1000) NOT NULL, 
+    dress_code VARCHAR(1000) NOT NULL,
+    catering VARCHAR(1000) NOT NULL,
+    price_range VARCHAR(1000) NOT NULL,
+    private_dining VARCHAR(1000) NOT NULL,
+    private_url VARCHAR(1000) NOT NULL,
+    latitude DECIMAL(10, 8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL,
+    addr VARCHAR(1000) NOT NULL,
+    neighborhood VARCHAR(1000) NOT NULL,
+    cross_street VARCHAR(1000) NOT NULL,
+    parking VARCHAR(1000) NOT NULL,
+    public_transport VARCHAR(1000) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Payment_Options (
+    id int NOT NULL AUTO_INCREMENT,
+    opt VARCHAR(1000) NOT NULL,
+    rest_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES General(id)
+);
+
+CREATE TABLE Cuisines (
+    id int NOT NULL AUTO_INCREMENT,
+    cuisine VARCHAR(1000) NOT NULL,
+    top_cuisine boolean NOT NULL DEFAULT 0,
+    rest_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES General(id)
+);
+
+CREATE TABLE Tags (
+    id int NOT NULL AUTO_INCREMENT,
+    tag VARCHAR(1000) NOT NULL,
+    top_tag boolean NOT NULL DEFAULT 0,
+    rest_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES General(id)
+);
+
+
+CREATE TABLE Hours (
+    id int NOT NULL AUTO_INCREMENT,
+    meal VARCHAR(1000) NOT NULL,
+    day_name VARCHAR(1000) NOT NULL,
+    time_range VARCHAR(1000) NOT NULL,
+    rest_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (rest_id) REFERENCES General(id)
+);
+
+
+
+
